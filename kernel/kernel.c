3a512a7f56da8cd8287a324ed83bae043e0294a0
Mikhail Sergeev <sergeevm100@gmail.com>
2023-12-06 19:36:25 +0300

    new: video-driver

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,11 +1,12 @@
-void 
-dummy_func()
-{
-
-}
+#include "../drivers/ports.h"
+#include "../drivers/screen.h"
 
 void main()
 {
-    char *video_memory = (char *) 0xb8000;
-    *video_memory = 'X';
+    clear_screen();
+    kprint_at("X", 1, 6);
+    kprint_at("This text spans multiple lines", 75, 10);
+    kprint_at("There is a line\nbreak", 0, 20);
+    kprint("There is a line\nbreak");
+    kprint_at("What happens when we run out of space?", 45, 24);
 }
